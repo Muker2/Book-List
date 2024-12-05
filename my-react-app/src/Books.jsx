acb132b3a46d3d3be6e0638682f01600c6e25519
Murad Kerimov <44784608+Muker2@users.noreply.github.com>
2024-12-05 21:21:04 +0100

    Added search function to sidebar instead of main content

--- my-react-app/src/Books.jsx
+++ my-react-app/src/Books.jsx
@@ -31,34 +31,26 @@ function Books() {
 
     return (
         <div className="bookContent">
-            <Sidebar className="sidebar"></Sidebar>
+            <Sidebar value={searchTerm} onChange={handleInputChange}
+                className="sidebar"></Sidebar>
             <div className="Mainbar">
-            <div className="bookListHeader">
-                <h2>From silent films to modern-day slashers, the world of horror cinema has evolved into a genre
-                     that both terrifies and captivates audiences. This collection delves deep into the artistry, cultural impact,
-                      and psychological power of horror films, exploring iconic monsters, groundbreaking directors, and th
-                      e fear-inducing techniques that have left viewers trembling for decades. Discover how horror on the
-                       big screen reflects society’s darkest fears and why we can’t look away.</h2>
-            </div>
-            <div className="books">
-                <input type="text" className="bookSearch"
-                    value={searchTerm}
-                    onChange={handleInputChange} />
+                <div className="bookListHeader">
 
-                <ul className="bookList">{searchBooks.map((book) =>
-                    <div className="bookEntry" key={book.id}>
-                        <div className="bookImage">
-                            <img src={book.volumeInfo.imageLinks?.smallThumbnail}></img></div>
-                        <div className="bookText">
-                            <h3>{book.volumeInfo.title}</h3>
-                            <h4>{book.volumeInfo.subtitle}</h4>
-                            <h5>{book.volumeInfo.description}</h5>
+
+                    <ul className="bookList">{searchBooks.map((book) =>
+                        <div className="bookEntry" key={book.id}>
+                            <div className="bookImage">
+                                <img src={book.volumeInfo.imageLinks?.smallThumbnail}></img></div>
+                            <div className="bookText">
+                                <h3>{book.volumeInfo.title}</h3>
+                                <h4>{book.volumeInfo.subtitle}</h4>
+                                <h5>{book.volumeInfo.description}</h5>
+                            </div>
                         </div>
-                    </div>
-                )}
-                </ul>
+                    )}
+                    </ul>
+                </div>
             </div>
-        </div>
         </div>)
 }
 
--- my-react-app/src/Sidebar.jsx
+++ my-react-app/src/Sidebar.jsx
@@ -1,13 +1,26 @@
-function Sidebar() {
+function Sidebar({value, onChange}) {
     return (
         <div className="sidebar">
+            <input type="text" className="bookSearch"
+                    value={value}
+                    onChange={onChange}
+            />
         <ul>
-            <li>Horror</li>
-            <li>Horror</li>
-            <li>Horror</li>
-            <li>Horror</li>
-            <li>Horror</li>
-            <li>Horror</li>
+            <li><a href="">Horror</a></li>
+            <li><a href="">Drama</a></li>
+            <li><a href="">Romance</a></li>
+            <li><a href="">Documentary</a></li>
+            <li><a href="">Comedy</a></li>
+            <li><a href="">Horror</a></li>
+            <li><a href="">Drama</a></li>
+            <li><a href="">Romance</a></li>
+            <li><a href="">Documentary</a></li>
+            <li><a href="">Comedy</a></li>
+            <li><a href="">Horror</a></li>
+            <li><a href="">Drama</a></li>
+            <li><a href="">Romance</a></li>
+            <li><a href="">Documentary</a></li>
+            <li><a href="">Comedy</a></li>
         </ul>
       </div>
     )
