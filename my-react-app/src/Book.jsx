ef4b3f89ce87a51a2d704a333f2be61ca5d67185
Murad Kerimov <44784608+Muker2@users.noreply.github.com>
2025-01-04 22:50:10 +0100

    Added styling to book details page

--- my-react-app/src/Book.jsx
+++ my-react-app/src/Book.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import useFetch from "./useFetch";
+import styles from "./Book.module.css";
 
 
 function Book() {
@@ -22,16 +23,31 @@ function Book() {
         return <p>No book details found</p>;
     }
 
-    return (
-    <div>
-        <img src={book.volumeInfo.imageLinks?.smallThumbnail}></img>
-        <h1>{book.volumeInfo.title}</h1>
-        <h3>{book.volumeInfo.authors}</h3>
-        <h3>Published by {book.volumeInfo.publisher} on {book.volumeInfo.publishedDate} </h3>
+    const publishedLanguage = book.volumeInfo.language;
+    const language = publishedLanguage.charAt(0).toUpperCase() + publishedLanguage.slice(1);
 
-        <h2>{book.volumeInfo.description}</h2>
+    return (
+        <div className={styles.bookDetails}>
+            <div className={styles.bookCover}>
+                <img src={book.volumeInfo.imageLinks?.smallThumbnail}></img>
+            </div>
+
+            <div className={styles.bookInfo}>
+                <div className={styles.bookTitle}>
+                    <h1>{book.volumeInfo.title}</h1>
+                    <h2>{book.volumeInfo.subtitle}</h2>
+                    <h3>{book.volumeInfo.authors}</h3>
+                    <h4>{book.volumeInfo.description}</h4>
+                </div>
+                <div className={styles.bookPublish}>
+                    <h3>Publisher {book.volumeInfo.publisher} </h3>
+                    <h3>Date {book.volumeInfo.publishedDate} </h3>
+                    <h3>Date {book.volumeInfo.printedPageCount} </h3>
+                    <h3>Language {language}</h3>
+                </div>
+            </div>
         </div>
-)
+    )
 }
 
 export default Book;
